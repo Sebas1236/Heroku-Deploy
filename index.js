78352e117c0d9da3eb35200e6e7f24002b02b778
Sebas1236 <45186829+Sebas1236@users.noreply.github.com>
2022-06-18 13:13:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.redirect('/index.html');
+  res.redirect('index.html');
 });
 
 app.listen(process.env.PORT || 5000);
